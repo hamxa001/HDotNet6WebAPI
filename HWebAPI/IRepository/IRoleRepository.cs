692577e522972ab71bdbe7fa7d8a634d7e3734e3
mhamzaj <mhamza.jawed@gmail.com>
2022-02-16 19:21:48 +0500

    Add insert roles method

--- HWebAPI/IRepository/IRoleRepository.cs
+++ HWebAPI/IRepository/IRoleRepository.cs
@@ -6,5 +6,6 @@ namespace HWebAPI.IRepository
     public interface IRoleRepository
     {
         public Task<ServiceResponse<List<IdentityRole>>> GetAllRoles();
+        public Task<ServiceResponse<List<IdentityRole>>> AddRoles(IEnumerable<IdentityRole> roles);
     }
 }
--- HWebAPI/Repository/RolesRepository.cs
+++ HWebAPI/Repository/RolesRepository.cs
@@ -1,7 +1,9 @@
-﻿using HWebAPI.IRepository;
+﻿using HWebAPI.Helpers;
+using HWebAPI.IRepository;
 using HWebAPI.Models;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using System.Linq;
 
 namespace HWebAPI.Repository
 {
@@ -21,6 +23,34 @@ namespace HWebAPI.Repository
                 ServiceResponse.Data = dbRoles;
                 ServiceResponse.Success = true;
                 ServiceResponse.Message = dbRoles.Count > 0 ? "All Roles Successfully Fetched!" : "No Roles Exist!";
+            }
+            catch (Exception ex)
+            {
+                ServiceResponse.Success = false;
+                ServiceResponse.Message = ex.Message;
+            }
+            return ServiceResponse;
+        }
+
+        public async Task<ServiceResponse<List<IdentityRole>>> AddRoles(IEnumerable<IdentityRole> roles)
+        {
+            var ServiceResponse = new ServiceResponse<List<IdentityRole>>();
+            try
+            {
+                foreach (var role in roles)
+                {
+                    role.Id = CommonCode.GuidID();
+                    role.ConcurrencyStamp = CommonCode.GuidID();
+                }
+
+                await _context.AddRangeAsync(roles);
+                await _context.SaveChangesAsync();
+
+                var listIds = roles.Select(x => x.Id).ToList();
+                                
+                ServiceResponse.Data = _context.Roles.Where(x => listIds.Contains(x.Id)).ToList();
+                ServiceResponse.Success = true;
+                ServiceResponse.Message = "Roles Successfuly inserted!";
             }
             catch (Exception ex)
             {
