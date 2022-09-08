6052752ee3e37251b3c239dcce7cf14bfa46f800
Hamza Jawed <hamzaj@humanforce.com>
2022-09-08 20:07:37 +0500

    add new login test class and login_success method

---
+++ HWebAPI_Test/TestControllers/TLogin.cs
@@ -0,0 +1,29 @@
+﻿using HWebAPI.Controllers;
+using HWebAPI.IRepository;
+using HWebAPI.Models;
+using HWebAPI.Repository;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HWebAPI_Test.TestControllers
+{
+    public class TLogin
+    {
+
+        [Fact]
+        public async Task login_Success()
+        {
+            //var loginController = new LoginController(new Mock<LoginRepository>().Object).ControllerContext;
+
+            //UserLogin userLogin = new UserLogin() { username = "login_User", password = "123456" };
+            
+            //var userLoginPostMethod = loginController.//Login(userLogin);
+
+            //Assert.NotNull(userLoginPostMethod);
+        }
+    }
+}
