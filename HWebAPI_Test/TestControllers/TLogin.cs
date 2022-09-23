a35ce5916b6fe8e5830c7c18d47ed1fe471558f8
Hamza Jawed <hamzaj@humanforce.com>
2022-09-23 20:48:55 +0500

    add successful unit test for login_Success in .net 6 project

--- HWebAPI_Test/TestControllers/TLogin.cs
+++ HWebAPI_Test/TestControllers/TLogin.cs
@@ -1,11 +1,14 @@
-﻿using HWebAPI.Controllers;
+﻿using Autofac.Extras.Moq;
+using HWebAPI.Controllers;
 using HWebAPI.IRepository;
 using HWebAPI.Models;
 using HWebAPI.Repository;
+using Microsoft.AspNetCore.Mvc;
 using Moq;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -13,17 +16,28 @@ namespace HWebAPI_Test.TestControllers
 {
     public class TLogin
     {
+        private LoginController _loginController;
+        private readonly Mock<ILoginRepository> _loginRepository = new Mock<ILoginRepository>();
+        private readonly Mock<LoginRepository> loginRepository = new Mock<LoginRepository>();
+        //private x mock;
+        public TLogin()
+        {
+            _loginController = new LoginController(_loginRepository.Object);
+        }
 
         [Fact]
         public async Task login_Success()
         {
-            //var loginController = new LoginController(new Mock<LoginRepository>().Object).ControllerContext;
+            ServiceResponse<string> serviceResponse = new ServiceResponse<string>() 
+            { Data = "Token", Message = "Login is Successfull!", Success = true };
 
-            //UserLogin userLogin = new UserLogin() { username = "login_User", password = "123456" };
-            
-            //var userLoginPostMethod = loginController.//Login(userLogin);
+            UserLogin userLogin = new UserLogin() { username = "login_User", password = "123456" };
 
-            //Assert.NotNull(userLoginPostMethod);
+            _loginRepository.Setup(x => x.Login(userLogin)).ReturnsAsync(serviceResponse);
+
+            ActionResult<ServiceResponse<string>> result = await _loginController.Login(userLogin);
+
+            Assert.IsType<ActionResult<ServiceResponse<string>>>(result);
         }
     }
 }
